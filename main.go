d621bd4f815cf5e9474f7234c88f7ea06f1341a3
Zander Mackie <zmackie@gmail.com>
2019-07-16 21:13:32 -0400

    Cleanup

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -18,25 +19,24 @@ func main() {
 	}
 	r, err := os.Open(flag.Arg(0))
 	if err != nil {
-		log.Fatal("Problem", err)
+		log.Fatal(err)
 	}
 
 	offset, err := strconv.Atoi(flag.Arg(1))
 	if err != nil {
-		log.Fatal("Problem", err)
+		log.Fatal(err)
 	}
-	// Possibly seek here?
+
 	name, err := NameAt(r, offset)
 	if err != nil {
-		log.Fatal("Problem", err)
+		log.Fatal(err)
 	}
 
-	fmt.Println("Name:", name)
-
+	fmt.Print(name)
 }
 
 func NameAt(r io.Reader, off int) (string, error) {
-	var name bytes.Buffer
+	var name []string
 	root := byte(0x00)
 	all, err := ioutil.ReadAll(r)
 	if err != nil {
@@ -44,60 +44,52 @@ func NameAt(r io.Reader, off int) (string, error) {
 	}
 	buf := bytes.NewReader(all)
 	buf.Seek(int64(off), 0)
+	if err != nil {
+		return "", err
+	}
 
 	lengthHeader, err := buf.ReadByte()
 	if err != nil {
 		return "", err
 	}
 
-	fmt.Println("LenghtHeader:", lengthHeader)
-
-	count := 0
 	for lengthHeader != root {
-		if count > 10 {
-			return "Probelm", fmt.Errorf("Problem with somethign")
-		}
-		isPointer := FetchOffset(int(lengthHeader))
-		if isPointer {
+		if IsPointer(int(lengthHeader)) {
 			offset, err := buf.ReadByte()
 			if err != nil {
 				return "", err
 			}
-			fmt.Println("Pointer! Readto:", offset)
-			// buf.Reset(all)
+
 			buf.Seek(int64(offset), 0)
 			if err != nil {
 				return "", err
 			}
+
 			lengthHeader, err = buf.ReadByte()
 			if err != nil {
 				return "", err
 			}
-			count++
 			continue
 		}
 
 		buffer := make([]byte, lengthHeader)
 		_, err := io.ReadFull(buf, buffer)
-		// if n != readTo {
-		// 	return "", fmt.Errorf("Something went wrong : %d", n)
-		// }
 		if err != nil {
-			return "", fmt.Errorf("Something else went wrong : %s", err)
+			return "", err
 		}
-		fmt.Println("Buffer", buffer)
-		name.Write(buffer)
-		name.WriteString(".") // how do we not do this on the last call
-		fmt.Println("Buffer", string(buffer))
+
+		name = append(name, string(buffer))
+
 		lengthHeader, err = buf.ReadByte()
 		if err != nil {
 			return "", err
 		}
 	}
-	return name.String(), nil
+
+	return strings.Join(name, "."), nil
 }
 
-func FetchOffset(b int) bool {
+func IsPointer(b int) bool {
 	pointerMask := 0xC0
 	return (b & pointerMask) == pointerMask
 }
--- main_test.go
+++ main_test.go
@@ -1,28 +1,58 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestNameCompressions(t *testing.T) {
-	t.Skip("Not Implemented")
+func TestNameAt(t *testing.T) {
+	var testNames = []struct {
+		bin      []byte
+		offset   int
+		expected string
+	}{
+		{
+			bin:      []byte{3, 110, 115, 49, 3, 99, 111, 109, 0},
+			offset:   0,
+			expected: "ns1.com",
+		},
+		{
+			bin:      []byte{0, 0, 0, 3, 110, 115, 49, 3, 99, 111, 109, 0},
+			offset:   3,
+			expected: "ns1.com",
+		},
+		{
+			bin:      []byte{0, 0, 0, 3, 110, 115, 49, 0, 3, 102, 111, 111, 192, 3},
+			offset:   8,
+			expected: "foo.ns1",
+		},
+	}
+	for _, tn := range testNames {
+		t.Run(tn.expected, func(t *testing.T) {
+			r := bytes.NewBuffer(tn.bin)
+			name, err := NameAt(r, tn.offset)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if name != tn.expected {
+				t.Fatalf("wanted %+v, got %+v\n", tn.expected, name)
+			}
+
+		})
+	}
 }
 
-func TestFetchOffset(t *testing.T) {
-	aPointer := 0xC6
-	anOffset := 6
-	notPointer := 0x06
+func TestIsPointer(t *testing.T) {
+	aPointer := 0xC0
+	notPointer := 0x6
 
-	offset, is := FetchOffset(aPointer)
+	is := IsPointer(aPointer)
 	if !is {
 		t.Fatal("Badd")
 	}
-	if offset != anOffset {
-		t.Fatal("Badd offset")
-	}
-	offset, is = FetchOffset(notPointer)
+
+	is = IsPointer(notPointer)
 	if is {
 		t.Fatal("Naddasdasd")
-	}
-	if offset != anOffset {
-		t.Fatal("Incorrect")
 	}
 }
